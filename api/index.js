8f2a1c7d1a55468699cbd2a1a7ecbcb9e7008b87
Ivan0301004 <ignizbones@gmail.com>
2026-07-24 20:51:05 -0600

    Open Graphs

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/open/server/server.mjs');
-  return reqHandler(req, res);
+  try {
+    const { reqHandler } = await import('../dist/open/server/server.mjs');
+    return reqHandler(req, res);
+  } catch (error) {
+    console.error('SSR Error:', error);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
 };
